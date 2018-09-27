daa4e2813d6dce91a11c2f755d7b0c1e4d31ccc6
DevChrome <40492004+DevChrome@users.noreply.github.com>
2018-09-27 13:01:18 +0530

    Update Noise.cs
    Changed cached colors to random colors.

--- Examples/Noise.cs
+++ Examples/Noise.cs
@@ -7,8 +7,6 @@ namespace Examples
 	{
 		private Random rnd = new Random();
 
-		private Pixel[] pixels = { Pixel.White, Pixel.Black, Pixel.Grey, Pixel.Red, Pixel.Green, Pixel.Blue, Pixel.Cyan, Pixel.Magenta, Pixel.Yellow };
-
 		static void Main(string[] args)
 		{
 			Noise n = new Noise();
@@ -20,7 +18,7 @@ namespace Examples
 		{
 			for (int i = 0; i < ScreenWidth / PixWidth; i++)
 				for (int j = 0; j < ScreenHeight / PixHeight; j++)
-					Draw(i, j, pixels[rnd.Next(pixels.Length)]);
+					Draw(i, j, Pixel.Random());
 		}
 	}
 }
