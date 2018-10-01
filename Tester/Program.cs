e87aa3d9a2ccad34cab9e21f9be6e2746a61c33c
Ritam <rattxz@gmail.com>
2018-10-01 21:33:30 +0530

    Safety commit

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Test
+namespace Tester
 {
 	public class Program : Game
 	{
@@ -14,20 +14,5 @@ namespace Test
 			p.Construct(500, 500, 5, 5);
 			p.Start();
 		}
-
-		private int size;
-		private Pixel col = Pixel.White;
-
-		public Program() => AppName = "Pixels";
-
-		public override void OnUpdate(TimeSpan elapsed)
-		{
-			Clear(Pixel.Black);
-			FillCircle(new Point(MouseX, MouseY), size * 2 + 1, col);
-		}
-
-		public override void OnMouseScroll() => size = Math.Abs(MouseScroll);
-		public override void OnMouseDown(Mouse m) => col = Pixel.Random();
-		public override void OnMouseRelease(Mouse m) => col = Pixel.White;
 	}
 }
