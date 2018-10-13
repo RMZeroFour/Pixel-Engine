8da23f9d29990c05761d9362c012f95297e599ec
DevChrome <40492004+DevChrome@users.noreply.github.com>
2018-10-13 23:17:21 +0530

    Update Noise.cs

--- Examples/Noise.cs
+++ Examples/Noise.cs
@@ -5,20 +5,23 @@ namespace Examples
 {
 	public class Noise : Game
 	{
-		private Random rnd = new Random();
-
 		static void Main(string[] args)
 		{
+			// Create an instance
 			Noise n = new Noise();
-			n.Construct(500, 500, 10, 10);
-			n.Start();
+			n.Construct(500, 500, 10, 10); // Construct the game
+			n.Start(); // Start and show a window
 		}
+
+		// Uncomment to make fullscreen
+		//public override void OnCreate() => Fullscreen();
 
 		public override void OnUpdate(TimeSpan elapsed)
 		{
+			// Loop through all the pixels
 			for (int i = 0; i < ScreenWidth / PixWidth; i++)
 				for (int j = 0; j < ScreenHeight / PixHeight; j++)
-					Draw(i, j, Pixel.Random());
+					Draw(i, j, Pixel.Random()); // Draw a random pixel
 		}
 	}
 }
