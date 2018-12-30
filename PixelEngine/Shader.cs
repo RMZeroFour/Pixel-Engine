c0c8a63e0d5068c263588afbde811b31b408b4b4
DevChrome <40492004+DevChrome@users.noreply.github.com>
2018-12-30 22:12:35 +0530

    Update Shader.cs

--- PixelEngine/Shader.cs
+++ PixelEngine/Shader.cs
@@ -1,11 +1,4 @@
 ﻿namespace PixelEngine
 {
-	public class Shader
-	{
-		public ShaderFunc Calculate { get; private set; }
-
-		public Shader(ShaderFunc calculate) => Calculate = calculate;
-	}
-
-	public delegate Pixel ShaderFunc(int x, int y, Pixel prev, Pixel cur);
+	public delegate Pixel Shader(int x, int y, Pixel prev, Pixel cur);
 }
