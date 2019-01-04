52c621a805b7414d6c06f2e4a57d685f57c476a1
DevChrome <40492004+DevChrome@users.noreply.github.com>
2019-01-04 13:35:52 +0530

    Fixes #3

--- Examples/WireWorld.cs
+++ Examples/WireWorld.cs
@@ -1,4 +1,5 @@
 using PixelEngine;
+using System;
 
 namespace Examples
 {
@@ -12,28 +13,25 @@ namespace Examples
 		// Is the game running?
 		private bool running;
 
-		static void Main(string[] args)
+		// The last position of mouse
+		private Point lastMousePos;
+
+		static void Main()
 		{
 			WireWorld ww = new WireWorld();
 			ww.Construct(75, 75, 10, 10, 30);
 			ww.Start();
 		}
 
-		// A cell can be either of these types
-		private enum Cell
-		{
-			Empty,
-			ElectronHead,
-			ElectronTail,
-			Conductor
-		}
-
 		public override void OnCreate()
 		{
-			// Init the state arrays
+			Enable(Subsystem.HrText);
+
+			// Init the cell arrays
 			current = new Cell[ScreenWidth, ScreenHeight];
 			next = new Cell[ScreenWidth, ScreenHeight];
 
+			// Write "WIREWORLD"
 			Set(15, 28, "#########################################");
 			Set(15, 29, "#.......................................#");
 			Set(15, 30, "+...#####.###..###.#...#####.###..#...##.");
@@ -41,20 +39,10 @@ namespace Examples
 			Set(15, 32, "#.#.#..#..###..###.#.#.#.#.#.###..#...#.#");
 			Set(15, 33, "#.#.#..#..#.#..#...#.#.#.#.#.#.#..#...#.#");
 			Set(15, 34, "##.##.#####..#.######.##.#####..#.######.");
+
 		}
 
-		public override void OnUpdate(float elapsed)
-		{
-			// Clear and render
-			Clear(Pixel.Presets.Black);
-			DrawCells();
-
-			// Update only if runnig
-			if (running)
-				UpdateCells();
-		}
-
-		// Utility to set cells
+		// A utility function to set cells using text
 		private void Set(int x, int y, string s)
 		{
 			for (int i = 0; i < s.Length; i++)
@@ -83,6 +71,45 @@ namespace Examples
 			}
 		}
 
+		public override void OnUpdate(float elapsed)
+		{
+			if (!running && GetMouse(Mouse.Any).Down == true)
+			{
+				Cell c = Cell.Empty;
+
+				// Left => Conductor
+				if (GetMouse(Mouse.Left).Down == true)
+					c = Cell.Conductor;
+				// Right => Empty
+				else if (GetMouse(Mouse.Right).Down == true)
+					c = Cell.Empty;
+				if (GetMouse(Mouse.Middle).Down == true)
+				{
+					// Middle + Shift => Tail
+					if (GetKey(Key.Shift).Down)
+						c = Cell.ElectronTail;
+					// Middle => Head
+					else
+						c = Cell.ElectronHead;
+				}
+
+				Place(lastMousePos.X, lastMousePos.Y, MouseX, MouseY, c);
+			}
+
+			// Clear and render
+			Clear(Pixel.Presets.Black);
+			DrawCells();
+
+			DrawTextHr(Point.Origin, running ? "Running" : "Paused", Pixel.Presets.White, 2);
+
+			// Update only if running
+			if (running)
+				UpdateCells();
+
+			// Capture the mouse position
+			lastMousePos = new Point(MouseX, MouseY);
+		}
+
 		private void UpdateCells()
 		{
 			// Get total count of neighbouring cells which are electron heads
@@ -92,22 +119,22 @@ namespace Examples
 
 				bool IsHead(int a, int b) => current[a, b] == Cell.ElectronHead;
 
-				if (x >= 0 && IsHead(x - 1, y)) // West
+				if (x > 0 && IsHead(x - 1, y)) // West
 					sum++;
 				if (x < ScreenWidth - 1 && IsHead(x + 1, y)) // East
 					sum++;
 
-				if (y >= 0 && IsHead(x, y - 1)) // North
+				if (y > 0 && IsHead(x, y - 1)) // North
 					sum++;
 				if (y < ScreenHeight - 1 && IsHead(x, y + 1)) // South
 					sum++;
 
-				if (x >= 0 && y >= 0 && IsHead(x - 1, y - 1)) // North West
+				if (x > 0 && y > 0 && IsHead(x - 1, y - 1)) // North West
 					sum++;
-				if (x < ScreenWidth - 1 && y >= 0 && IsHead(x + 1, y - 1)) // North East
+				if (x < ScreenWidth - 1 && y > 0 && IsHead(x + 1, y - 1)) // North East
 					sum++;
 
-				if (x >= 0 && y < ScreenHeight - 1 && IsHead(x - 1, y + 1)) // South West
+				if (x > 0 && y < ScreenHeight - 1 && IsHead(x - 1, y + 1)) // South West
 					sum++;
 				if (x < ScreenWidth - 1 && y < ScreenHeight - 1 && IsHead(x + 1, y + 1)) // South East
 					sum++;
@@ -150,6 +177,7 @@ namespace Examples
 			for (int i = 0; i < ScreenWidth; i++)
 				for (int j = 0; j < ScreenHeight; j++)
 					current[i, j] = next[i, j];
+
 		}
 
 		private void DrawCells()
@@ -167,24 +195,40 @@ namespace Examples
 			}
 		}
 
-		public override void OnMouseDown(Mouse m)
+		// Place cells with the mouse using the Bresenham algorithm
+		private void Place(int x, int y, int x2, int y2, Cell cell)
 		{
-			if (!running)
+			int w = x2 - x;
+			int h = y2 - y;
+			int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
+			if (w < 0) dx1 = -1; else if (w > 0) dx1 = 1;
+			if (h < 0) dy1 = -1; else if (h > 0) dy1 = 1;
+			if (w < 0) dx2 = -1; else if (w > 0) dx2 = 1;
+			int longest = Math.Abs(w);
+			int shortest = Math.Abs(h);
+			if (!(longest > shortest))
 			{
-				// Place conductors
-				if (m == Mouse.Left)
-					current[MouseX, MouseY] = Cell.Conductor;
-				// Clear cell
-				else if (m == Mouse.Right)
-					current[MouseX, MouseY] = Cell.Empty;
-				if (m == Mouse.Middle)
+				longest = Math.Abs(h);
+				shortest = Math.Abs(w);
+				if (h < 0) dy2 = -1; else if (h > 0) dy2 = 1;
+				dx2 = 0;
+			}
+			int numerator = longest >> 1;
+			for (int i = 0; i <= longest; i++)
+			{
+				current[x, y] = cell;
+
+				numerator += shortest;
+				if (!(numerator < longest))
 				{
-					// Middle + Shift => Tail
-					if (GetKey(Key.Shift).Down)
-						current[MouseX, MouseY] = Cell.ElectronTail;
-					// Middle => Head
-					else
-						current[MouseX, MouseY] = Cell.ElectronHead;
+					numerator -= longest;
+					x += dx1;
+					y += dy1;
+				}
+				else
+				{
+					x += dx2;
+					y += dy2;
 				}
 			}
 		}
@@ -193,10 +237,7 @@ namespace Examples
 		{
 			// Pause game
 			if (k == Key.Enter)
-			{
 				running = !running;
-				AppName = "WireWorld " + (running ? "Running" : "Paused");
-			}
 
 			// Simulate next frame
 			if (k == Key.F)
@@ -209,6 +250,15 @@ namespace Examples
 					for (int j = 0; j < ScreenHeight; j++)
 						current[i, j] = next[i, j] = Cell.Empty;
 			}
+		}
+
+		// A cell can be either of these types
+		private enum Cell
+		{
+			Empty,
+			ElectronHead,
+			ElectronTail,
+			Conductor
 		}
 	}
 }
